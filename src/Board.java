251fb4991958c17fab968efd09607615d3cceff2
Kemal Demirtas <demirtas@kth.se>
2017-10-05 09:13:36 +0200

    Add Board and Main object

---
+++ src/Board.java
@@ -0,0 +1,12 @@
+public class Board {
+    // Makes the board
+    private int lenght = 20;
+    private int widht = 20;
+    private int[][] board = new int[lenght][widht];
+
+    private void fillBoard(){
+
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Make board object
+        Board board = new Board();
+
+
+
+        // Make player object
+
+
+
+        // Make monster objects
+
+
+
+        // Run game until player dead
+
+
+
+        // Game over
+
+
+
+
+    }
+}
